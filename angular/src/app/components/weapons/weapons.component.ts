17c2e0c7733f0762c3f28b5c0f8538a3091ebf75
Kyle Carhart <KMCGamer@live.com>
2018-05-16 17:03:12 -0400

    turn on/of table headers

--- angular/src/app/components/weapons/weapons.component.ts
+++ angular/src/app/components/weapons/weapons.component.ts
@@ -5,6 +5,18 @@ import { WeaponsService } from '../../services/weapons.service';
 import { ActivatedRoute } from '@angular/router';
 import { map } from "rxjs/operators";
 
+const HEADER_ORDER = [
+  "name",
+  "category",
+  "type",
+  "damage",
+  "mobility",
+  "range",
+  "recoil",
+  "fireRate",
+  "accuracy"
+];
+
 @Component({
   selector: 'app-guns',
   templateUrl: './weapons.component.html',
@@ -14,6 +26,18 @@ export class WeaponsComponent implements OnInit {
   weapons: Weapon[];
   tableView: boolean;
   currentTab: string;
+  headers: string[];
+  allHeaders = [
+    "name",
+    "category",
+    "type",
+    "damage",
+    "mobility",
+    "range",
+    "recoil",
+    "fireRate",
+    "accuracy"
+  ];
 
   constructor(
     private weaponsService: WeaponsService,
@@ -22,6 +46,7 @@ export class WeaponsComponent implements OnInit {
   ngOnInit() {
     this.currentTab = 'all';
     this.tableView = false;
+    this.headers = ["name", "category", "type"];
     this.route.queryParamMap.subscribe(paramMap => {
       this.weaponsService.queryWeapons(paramMap).subscribe(weapons => {
         this.weapons = weapons;
@@ -47,5 +72,18 @@ export class WeaponsComponent implements OnInit {
   /* TODO: Fix current tabs */
   public isCurrentTab(tab: string): boolean {
     return this.currentTab === tab;
+  }
+
+  public toggleHeader(header: string) : void {
+    if (this.headers.indexOf(header) !== -1) {
+      this.headers.splice(this.headers.indexOf(header), 1)
+      // delete this.headers[this.headers.indexOf(header)];
+    } else {
+      const index = HEADER_ORDER.indexOf(header);
+      this.headers.push(header);
+      this.headers.sort((a, b) => {
+        return HEADER_ORDER.indexOf(a) - HEADER_ORDER.indexOf(b);
+      });
+    }
   }
 }
